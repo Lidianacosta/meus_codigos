ce006c2e510d2156bb8c74643f545053feec59b3
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-10 13:39:17 -0300

    Update Labq1.c

--- lab estrutura I/Labq1.c
+++ lab estrutura I/Labq1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<locale.h>
 
 typedef struct dados_funcionarios {
     char nome[50];
@@ -15,6 +15,7 @@ void modifica_salario(df *f,int qtd);
 void verificar_salario(df *f,int qtd);
 
 int main() {
+    setlocale(LC_ALL,"Portuguese");
     int qtd;
     printf("digite o numero de funcionários que deseja cadastrar: ");
     scanf("%d",&qtd);
@@ -28,7 +29,6 @@ int main() {
 
     free(f);
 }
-
 
 void preencher(df *f,int qtd) {
     int i;
@@ -61,7 +61,7 @@ void imprimir(df *f,int qtd) {
 void modifica_salario(df *f,int qtd) {
     int i,op;
 
-    printf("deseja muda o salario do funcionario se sim digite 1 se nao digite 0: ");
+    printf("deseja muda o salario do funcionario ?\n se sim, digite 1 se nao, digite 0: ");
     scanf("%d",&op);
 
     if(op == 0) {
@@ -72,7 +72,6 @@ void modifica_salario(df *f,int qtd) {
             scanf("%f",&f[i].salario);
         }
     }
-
     if(op !=0) {
         printf("salário depois do reajuste\n");
         imprimir(f,qtd);
@@ -84,22 +83,25 @@ void verificar_salario(df *f,int qtd) {
     int idx_maior, idx_menor,i;
     float veri_maior,veri_menor;
 
-
     veri_maior = f[0].salario;
     idx_maior = 0;
     veri_menor = f[0].salario;
     idx_menor = 0;
 
     for(i=1; i<qtd; i++) {
-
         if(veri_maior < f[i].salario) {
             idx_maior = i;
         }
-
         if(veri_menor < f[i].salario) {
             idx_menor =i;
         }
     }
-    printf("quem recebe mais e o %s\n",f[idx_maior].cargo);
-    printf("quem recebe menos e o %s\n",f[idx_menor]. cargo);
+
+    if (f[idx_maior].cargo == f[idx_menor]. cargo)
+    {
+       printf("quem recebe mais é o %s\n",f[idx_maior].cargo);
+    }else{  
+    printf("quem recebe mais é o %s\n",f[idx_maior].cargo);
+    printf("quem recebe menos é o %s\n",f[idx_menor]. cargo);
+   }
 }
