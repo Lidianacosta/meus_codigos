f3738e8a5a78744b52c706796573d65654b20af6
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-13 13:42:23 -0300

    Update matricula_turma.c

--- estrutura I/matricula_turma.c
+++ estrutura I/matricula_turma.c
@@ -40,7 +40,7 @@ Turma *procura_turma(Turma **turmas, int n, char id);
 
 int main()
 {
-    int op, i, count;
+    int op, i = 0, count, cont_alunos, mat;
     char id;
 
     Turma **turmas = (Turma **)malloc(MAX_TURMAS * sizeof(Turma *));
@@ -60,9 +60,16 @@ int main()
         {
 
         case 1:
-            printf("digite o id da turma que deseja cadastrar: ");
-            scanf(" %c", turmas[i]->id);
-            turmas[i] = cria_turma(turmas[i]->id);
+            if (i < MAX_TURMAS)
+            {
+                printf("digite o id da turma que deseja cadastrar: ");
+                scanf(" %c", turmas[i]->id);
+                turmas[i] = cria_turma(turmas[i]->id);
+            }
+            else
+            {
+                printf("nao ha mais vagas para criar turmas\n");
+            }
 
             break;
         case 2:
@@ -71,11 +78,34 @@ int main()
         case 3:
             printf("digite o id da  turma que deseja cadastrar o aluno: ");
             scanf(" %c", &id);
-            printf("digite a matricula do aluno: ");
-            scanf("%d", turmas[i]->alunos[i]->mat);
-            printf("digite o nome do aluno: ");
-            scanf(" %[^\n]s", turmas[i]->alunos[i]->nome);
-            matricula_aluno(turmas, turmas[i]->alunos[i]->mat, turmas[i]->alunos[i]->nome);
+            for (count = 0; count < MAX_TURMAS; count++)
+            {
+                if (turmas[count]->id == id)
+                {
+                    printf("digite a matricula do aluno: ");
+                    scanf("%d", &mat);
+
+                    for (cont_alunos = 0; cont_alunos < MAX_VAGAS; cont_alunos++)
+                    {
+                        {
+                            if (turmas[count]->alunos[cont_alunos] == NULL)
+                            {
+                                turmas[count]->alunos[cont_alunos]->mat = mat;
+                                matricula_aluno(turmas[count], turmas[count]->alunos[cont_alunos]->mat, turmas[count]->alunos[cont_alunos]->nome);
+                            }
+                            else
+                            {
+                                printf("nao a mais vagas para cadastra o aluno na turma");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    printf("Turma inexistente!\n");
+                }
+            }
+
             break;
         case 4:
 
@@ -125,8 +155,8 @@ Turma *cria_turma(char id)
 
 void matricula_aluno(Turma *turma, int mat, char *nome)
 {
-    turma
-
+    printf("digite o nome do aluno : ");
+    scanf(" %[^\n]s", nome);
 }
 
 void lanca_notas(Turma *turma)
