46c9e4bbf53095e0bbdd6d3de14c943ec9c25d0e
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-15 10:25:58 -0300

    Update àrea e perímetro-de-um-hexagono.c

--- lab estrutura I/àrea e perímetro-de-um-hexagono.c
+++ lab estrutura I/àrea e perímetro-de-um-hexagono.c
@@ -4,35 +4,30 @@
 // void calcula_hexagono(float l, float *area, float *perimetro); A área e o perímetro de um
 // hexágono regular são dados, respectivamente, por:
 //            A = (3*l*l*sqrt(3))/2
-//            P =6*l        
-//
+//            P = 6*l
+//.
 
+#include <stdio.h>
+#include <math.h>
 
-#include<stdio.h>
-#include<math.h>
+void calcula_hexagono(float l, float *area, float *perimetro); // funçâo.
 
+int main()
+{
 
-void calcula_hexagono(float l, float *area, float *perimetro);
+    float A, P, l; // declarando as variaveis
 
-int main(){
+    printf("digite o lado do hexagono\n"); // pedindo e lendo o lado do hexágono.
+    scanf("%f", &l);
 
-float A,P,l;
+    calcula_hexagono(l, &A, &P); // chamada da função passando uma variavel simples e dois endereço.
 
-printf("digite o lado do hexagono\n");
-scanf("%f",&l);
+    printf("A: %.2f  P: %.2f", A, P); // mostrando a área e perimetro ao úsuario.
 
-calcula_hexagono(l,&A,&P);
-
-printf("A: %f P: %f",A,P);
-
-    return(0);
+    return (0);
 }
 
-void calcula_hexagono(float l, float *area, float *perimetro){
-
-
-*area = (3*pow(2,l)*sqrt(3))/2;
-*perimetro = 6*l ;
-
-
+void calcula_hexagono(float l, float *area, float *perimetro){// a função recebe a copia do conteudo e dois endereço para poder modificar o valor da variavel tbm na main.
+    *area = (3 * pow(2, l) * sqrt(3)) / 2; // calculando a área e armazenando em A.
+    *perimetro = 6 * l;                    // calculando a o perimetro e armazenando em P.
 }
