b23c4a3908feae38d9b012e95f88aab9979c9807
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-24 13:36:24 -0300

    Update alua_dia16,02_QUESTAO02.c

--- lab estrutura I/alua_dia16,02_QUESTAO02.c
+++ lab estrutura I/alua_dia16,02_QUESTAO02.c
@@ -2,16 +2,18 @@
 // tipo deve ser armazenado como uma union que armazena um dos tipos: ALIMENTO, BEBIDA ou
 // ELETRONICO. O programa deve ler os dados de um produto e imprimir os dados do produto
 
+// nao entendi direito oq a questao queria entao fiz assim
+
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef union escolha{
+typedef union escolha{     // para a pessoa escolher so um 
     char alimento[20];
     char bebida[20];
     char eletronico[20];
 }Escolha;
 
-typedef struct produto{
+typedef struct produto{     // para guarda dados de um produto 
     char nome[20];
     float preco;
     Escolha escolha
@@ -20,42 +22,42 @@ typedef struct produto{
 
 int main(void){
     int op;
-    Produto produto;
+    Produto produto;    // variavel do tipo produto 
 
     printf("digite o nome do produto");
-    scanf(" %[^\n]s",produto.nome);
+    scanf(" %[^\n]s",produto.nome);     //pedinso nome do produto 
     
     printf("digite o produto que vai querer \n");
-    printf("1 para alimwnto 2 para bebida e 3 para eletronico : ");
+    printf("1 para alimwnto 2 para bebida e 3 para eletronico : "); // dando um mini menu e pedindo a op 
     scanf("%d",&op);
 
         
 
-    switch (op)
+    switch (op) // verificando qual op a pessoa escolheu
     {
     case 1:
             printf("digite o alimento : ");
-            scanf(" %[^\n]s",produto.escolha.alimento);
+            scanf(" %[^\n]s",produto.escolha.alimento);     //se o produto e um alimneto 
     break;
     case 2 :
             printf("digite a bebida : ");
-            scanf(" %[^\n]s",produto.escolha.bebida);
+            scanf(" %[^\n]s",produto.escolha.bebida);       //se o produto e uma bebida
     break;
     case 3 :
             printf("digite o eletronico : ");
-            scanf(" %[^\n]s",produto.escolha.eletronico);
+            scanf(" %[^\n]s",produto.escolha.eletronico);   //se o produto e um eletronico
     break;
     default:
-        printf("opçao invalida\n");
+        printf("opçao invalida\n");     // se a op nao for valida
         exit(1);
         break;
     }
 
-    printf("digite o preço : ");
-    scanf("%f",&produto.preco);
+    printf("digite o preço : ");    // o preço do produto 
+    scanf("%f",&produto.preco);  
 
     printf("DADOS : \n");
-    switch (op)
+    switch (op)             /// vericar qual foi a op para mostra se um alimento , uma bebida ou um eletronico 
     {
     case 1:
 
@@ -69,6 +71,6 @@ int main(void){
     break;
     }
 
-    printf("preço : %.2f",produto.preco);
+    printf("preço : %.2f",produto.preco);       // mostrando o preço do produto 
     return (0);
 }
