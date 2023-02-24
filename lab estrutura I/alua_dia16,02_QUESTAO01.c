3b2e44aa513bee9aacee2ad7e9090fd0bf95a57a
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-02-24 13:27:48 -0300

    Update alua_dia16,02_QUESTAO01.c

--- lab estrutura I/alua_dia16,02_QUESTAO01.c
+++ lab estrutura I/alua_dia16,02_QUESTAO01.c
@@ -5,11 +5,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define numero_pessoas 1
+#define numero_pessoas 1 // costante para o numero de pessoas 
 
-typedef enum sexo{MASCULINO,FEMININO}Sexo;
+typedef enum sexo{MASCULINO,FEMININO}Sexo; // enum para o genero do passoa 
 
-typedef struct pessoas{
+typedef struct pessoas{ // para guarda os dados das pessoas  
     char nome[50];
     int idade;
     Sexo sexo;
@@ -17,24 +17,26 @@ typedef struct pessoas{
 
 int main(void){
 
-    Pessoas *pessoas = (Pessoas*)malloc( numero_pessoas* sizeof(Pessoas));
+    Pessoas *pessoas = (Pessoas*)malloc( numero_pessoas* sizeof(Pessoas));  // alocando dinamicamente 
 
-    printf("digite o nome : ");
+    printf("digite o nome : ");         // pedindo o nome 
     scanf(" %[^\n]s",pessoas->nome);
-    printf("digite a idade: ");
+    printf("digite a idade: ");        // pedindo a idade 
     scanf("%d",&pessoas->idade);
-    printf("digite o genero: ");
+    printf("digite o genero: ");          //pedindo o genero
     scanf("%d",(int*)&pessoas->sexo);
 
-    printf("digite o nome : %s\n",pessoas->nome);
-    printf("digite a idade: %d\n",pessoas->idade);
+    printf(" o nome : %s\n",pessoas->nome);  // mostrando os dados 
+    printf("a idade: %d\n",pessoas->idade);
  
   //  pessoas->sexo == MASCULINO?printf("MASCULINO\n"):printf("FEMININO\n");
  
-    pessoas->sexo == FEMININO?printf("FEMININO\n"):printf("MASCULINO\n");
+    pessoas->sexo == FEMININO?printf("FEMININO\n"):printf("MASCULINO\n");   // verificando se é homem ou mulher 
  
-  //  printf("digite o genero: %d\n",pessoas->sexo);
+  //  printf("o genero: %d\n",pessoas->sexo);
    
-    printf("");
+   // printf("");
+    
+    // nao terminei de ajeitar 
     return(0);
 }
