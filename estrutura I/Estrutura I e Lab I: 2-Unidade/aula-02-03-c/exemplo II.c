6959019f19c0bb1576a051805d2351edf37821d3
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-03 10:23:41 -0300

    Update exemplo II.c

--- estrutura I/Estrutura I e Lab I: 2-Unidade/aula-02-03-c/exemplo II.c
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/aula-02-03-c/exemplo II.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<ctype.h>
 
 int main(void){
-
     int c;
     char entrada[121];
     char saida[121];
@@ -12,31 +10,27 @@ int main(void){
 
     printf("digite o nome do arquivo de entrada: ");
     scanf("%120s", entrada);
-    printf("digite o nome  do arquivo de saida: ");
-    scanf("%120s", saida); 
-
+    printf("digite o nome do arquivo de saida: ");
+    scanf("%120s", saida);
+    
     e = fopen(entrada, "rt");
     if(e == NULL){
-        printf("nao foi possivel abrir o arquivo de entrada\n");
+        printf("Nao foi possivel abrir arquivo de entrada.\n");
         return(1);
     }
-
-    s = fopen(saida,"wt");
+    s = fopen(saida, "wt");
     if(s == NULL){
-        printf("nao foi possivel abrir o arquivo de saida\n");
-       fclose(e);       
+        printf("Nao foi possivel abrir arquivo de saida.\n");
+        fclose(e);
         return(1);
     }
 
-    while((c = fgetc(e)) != EOF){
+    while ((c = fgetc(e)) !=EOF){
         fputc(toupper(c), s);
-
-        fclose(e);
-        fclose(s);
-        
-      
     }
 
-
+    fclose(e);
+    fclose(s);
+    
     return(0);
 }
