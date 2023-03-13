792ee8857835424c5e4bce767392b0243a249df0
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-13 13:18:49 -0300

    Create contabancaria.c

---
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/01-ContaBancaria/contabancaria.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct contabancaria{ 
+    char titular[51];
+    int numero_conta;
+    float saldo;
+}ContaBancaria;
+
+ContaBancaria* cria_conta(char* titular, int numero_conta, float saldo){
+    ContaBancaria* conta = (ContaBancaria*)malloc(sizeof(ContaBancaria));
+    if (conta == NULL){
+        printf("erro sem espaço na memoria!\n");
+        exit(1);
+    }
+    strcpy(conta->titular,titular);
+    conta->numero_conta = numero_conta;
+    conta->saldo = saldo;
+    return(conta);
+}
+
+void deposita(ContaBancaria* conta, float valor_deposita){
+    conta->saldo += valor_deposita;
+}
+
+void saca(ContaBancaria* conta, float valor_sacar){
+    if (conta->saldo >=valor_sacar){
+        conta->saldo -=valor_sacar;
+    }else{
+        printf("saldo insufifiente!\n");
+    }
+}
+
+void transfere(ContaBancaria* conta_trasfere, ContaBancaria* conta_recebe, float valor_trasferencia){
+    if (conta_trasfere->saldo >=valor_trasferencia){
+        conta_trasfere->saldo -= valor_trasferencia;
+        conta_recebe->saldo += valor_trasferencia;
+    }else{
+        printf("saldo insufifiente!\n");
+    }
+}
+
+float saldo(ContaBancaria* conta){
+    return(conta->saldo);
+}
+
+void exclui_conta(ContaBancaria* conta){
+    free(conta);
+}
