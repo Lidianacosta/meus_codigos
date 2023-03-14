3d60a36da033059b4d0126c96edcf4c81c2acdd8
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-14 10:32:32 -0300

    Update main.c

--- estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/02-alunos/main.c
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/02-alunos/main.c
@@ -1,1 +1,40 @@
+#include<stdio.h>
+#include"aluno.c"
+#include<stdlib.h>
 
+
+#define QTD_ALUNOS 2
+
+int main(void){
+    int cont_alunos_criados = 0;
+    Aluno **alunos = (Aluno**)malloc(QTD_ALUNOS* sizeof(Aluno*));
+    if (alunos == NULL){
+        printf("erro!\n");
+        exit(1);
+    }
+
+    Aluno *aluno_procurado;
+
+    char nome[] ="lidiana";
+
+    alunos[cont_alunos_criados] = aluno_cria(nome , 10);
+    cont_alunos_criados++;
+    aluno_procurado =  procura_Aluno(alunos,nome);
+    aluno_imprime(aluno_procurado);
+
+    aluno_libera();
+
+    return(0);
+}   
+    // do
+    // {
+    //        switch (op)
+    // {
+    // case /* constant-expression */:
+    //     /* code */
+    //     break;
+    
+    // default:
+    //     break;
+    // } 
+    // } while (op != 6);
