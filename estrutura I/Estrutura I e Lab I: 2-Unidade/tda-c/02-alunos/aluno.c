4ddf88a6b32da071d0b984e2a7995b12a0b29d89
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-18 13:23:47 -0300

    Atualizar o aluno.c
    faltou ordenar o vetor

--- estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/02-alunos/aluno.c
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/02-alunos/aluno.c
@@ -1,52 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "aluno.h"
 
 typedef struct aluno{
-    char nome[50];
-    float nota;
+  char nome[5];
+  float nota;
 }Aluno;
 
 Aluno* aluno_cria(char* nome, float nota){
-    Aluno* novo_aluno = (Aluno*)malloc(sizeof(Aluno));
-    if (novo_aluno == NULL){
-        printf("erro!\n");
-    }
-    strcpy(novo_aluno->nome,nome);
-    novo_aluno->nota = nota;
-    return(novo_aluno);
+  Aluno* novo_aluno =(Aluno*)malloc(sizeof(Aluno));
+  if(novo_aluno == NULL){
+    printf("erro\n");
+    exit(1);
+  }
+  strcpy(novo_aluno->nome,nome);
+  novo_aluno->nota = nota;
+  printf("Aluno cadastrado com sucesso\n");
+  return (novo_aluno);
 }
+
 void aluno_libera(Aluno* a){
-    free(a);
+  free(a);
 }
 
 void aluno_imprime(Aluno* a){
-    printf("Dados:\nnome: %s\nnota: %.2f",a->nome,a->nota);
+  printf("%s: %.2f\n",a->nome, a->nota);
 }
 
-// int compararNome(const void *a, const void *b){
-//     return(strcmp(((Aluno *)a)->nome,((Aluno *)b)->nome));
-// }
+void aluno_ordena(int n, Aluno** v); 
 
-// void aluno_ordena(int n, Aluno** v){
-//       qsort(v, 5, sizeof(Aluno), compararNome);
-// }
 void aluno_salva(FILE* fp, int n, Aluno** v){
-    int i;
-    for(i = 0; i < n; i++){
-        fprintf(fp, "%s\t%.2f\n", (*v)->nome, (*v)->nota);
-    }
+  int i;
+  for(i=0; i<n; i++){
+   fprintf(fp, "%s: %.2f\n", v[i]->nome, v[i]->nota);
+  }
 }
-
-Aluno *procura_Aluno(Aluno **alunos,int n, char nome)    // uma funçao que ajuda a achar uma turma pelo seu id 
-{
-    int cont_alunos;
-
-    for (cont_alunos = 0; cont_alunos < n; cont_alunos++){   // verifica se o id dado é igual ao da turma 
-        if (strcmp(alunos[cont_alunos]->nome,nome) == 0){
-            return (alunos[cont_alunos]); // retorna a turma encontrada 
-        }
-    }
-    return (NULL);  // retorna null se nao for encontrada a turma 
-}
-
