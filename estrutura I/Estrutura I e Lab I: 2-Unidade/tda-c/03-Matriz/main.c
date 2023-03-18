5122998845c6ca3dacfd40ce83d8f1f823b46db0
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-18 17:41:05 -0300

    Atualizar o main.c
    faltou a struct que tem o vetor **v

--- estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/03-Matriz/main.c
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/03-Matriz/main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matriz.h"
 
+int main(void) {
+  Matriz* mat = mat_cria(3,3);
+  mat_atribui(mat,2,2,5);
+  printf("%.2f\n", mat_acessa(mat,2,2));
+  printf("tem %d linha/as\n",mat_linhas(mat));
+  printf("tem %d coluna/as\n",mat_colunas(mat));
+  mat_libera(mat);
+  return 0;
+}
