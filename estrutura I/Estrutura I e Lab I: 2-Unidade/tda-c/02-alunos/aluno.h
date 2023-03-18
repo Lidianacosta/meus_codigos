23f088b210a5fc66baa9a3b6e88a33a5ca5535c4
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-18 13:24:40 -0300

    Atualizar o aluno.h
    faltou ordenar o vetor

--- estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/02-alunos/aluno.h
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/02-alunos/aluno.h
@@ -1,12 +1,15 @@
- /* Arquivo aluno.h */
+/* Arquivo aluno.h */
+
 #include <stdio.h>
 
 typedef struct aluno Aluno;
-Aluno* aluno_cria(char* nome, float nota);
-void aluno_libera(Aluno* a);
-void aluno_imprime(Aluno* a);
-//void aluno_ordena(int n, Aluno** v);
-void aluno_salva(FILE* fp, int n, Aluno** v);
-Turma *procura_Aluno(Turma **alunos, int n, char id);
 
-// int compararNome(const void *a, const void *b); 
+Aluno* aluno_cria(char* nome, float nota);
+
+void aluno_libera(Aluno* a);
+
+void aluno_imprime(Aluno* a);
+
+void aluno_ordena(int n, Aluno** v); 
+
+void aluno_salva(FILE* fp, int n, Aluno** v);
