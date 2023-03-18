c1207b1a2c8db18455ce1d1bde169cb4d29204db
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-18 13:08:06 -0300

    Atualizar o main.c
    modificações

--- estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/01-ContaBancaria/main.c
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/01-ContaBancaria/main.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
-#include "contabancaria.c"
-
-
+#include "contabancaria.h"
 
 int main(void){
     ContaBancaria* conta01;
     ContaBancaria* conta02;
     char nome01[20] = "lidiana", nome02[20] = "lucas";
-    conta01 = cria_conta(nome01,01,200);
+    conta01 = cria_conta(nome01,01,500);
     conta02 = cria_conta(nome02,02,200);
     deposita(conta01,200);
     deposita(conta02,200);
     saca(conta01,100);
     transfere(conta01,conta02,100);
-    printf("%s seu saldo é: %.2f\n",conta01->titular,saldo(conta01));
-    printf("%s seu saldo é: %.2f\n",conta02->titular,saldo(conta02));
+    imprimir(conta01);
+    imprimir(conta02);
+    exclui_conta(conta01);
+    exclui_conta(conta02);
     return(0);
 }
