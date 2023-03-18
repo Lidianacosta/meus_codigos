0c2dd8b9050b25dff8913709bd89db713fc71ffc
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-03-18 17:44:14 -0300

    Atualizar o matriz.h
    faltou a struct que tem o vetor **v

--- estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/03-Matriz/matriz.h
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/tda-c/03-Matriz/matriz.h
@@ -1,1 +1,13 @@
+typedef struct matriz Matriz;
 
+Matriz* mat_cria(int m, int n);
+
+void mat_libera(Matriz* mat);
+
+float mat_acessa(Matriz* mat, int i, int j);
+
+void mat_atribui(Matriz* mat, int i, int j, float v);
+
+int mat_linhas(Matriz* mat);
+
+int mat_colunas(Matriz* mat); 
