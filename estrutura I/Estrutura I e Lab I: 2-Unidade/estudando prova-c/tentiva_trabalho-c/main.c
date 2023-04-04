fffcd01a8fb6a235e67a4ac9fcd9c4b8dfd8d658
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-04-04 14:04:43 -0300

    Update main.c

--- estrutura I/Estrutura I e Lab I: 2-Unidade/estudando prova-c/tentiva_trabalho-c/main.c
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/estudando prova-c/tentiva_trabalho-c/main.c
@@ -6,9 +6,17 @@
 int main(void){
     Aluno* alunos = (Aluno*)malloc(sizeof(Aluno));
     int qtd_alunos,op,idx,mat;
-    qtd_alunos = ler_aluno(alunos);
+    char nome[50];
+        FILE* arquivo = fopen("alunos.txt", "rt");
+    if (arquivo == NULL){
+        printf("erro!");
+        exit(1);
+    }
+    fscanf(arquivo, "%d",&qtd_alunos);
+    fclose(arquivo);
+    alunos = realloc(alunos,sizeof(Aluno)*(qtd_alunos));
+    ler_aluno(alunos);
     printf("\ntem %d alunos cadastrados!\n",qtd_alunos);
-    
     
     do{
         menu();
@@ -23,7 +31,15 @@ int main(void){
             printf("\ntem %d alunos cadastrados!\n",qtd_alunos);
             break;
         case 2:
-            
+            printf("digite o nome do aluno que deseja busca: ");
+            scanf(" %[\n]s",nome);
+            idx = busca_interpolada_nome(alunos,qtd_alunos,nome);
+            printf("aaaaw!\n");
+            if (idx == -1){
+                printf("aluno nao encontrado\n");
+            }else{
+                printf("nome: %s , documento: %d , matricula: %d\n",alunos[idx].nome,alunos[idx].doc,alunos[idx].mat);
+            }
             break;
         case 3:
         printf("digite a matricula do aluno que deseja busca: ");
@@ -39,12 +55,14 @@ int main(void){
         case 4:
             imprime_alunos(alunos,qtd_alunos);
             break;
-        
+        case 5:
+            salva_alunos(alunos,qtd_alunos);
+            break;
         default:
             break;
         }
        
-    } while (op != 5);
+    } while (op != 6);
     
     return (0);
 }
