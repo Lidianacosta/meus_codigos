a4ebc2c27b7c7ebf6df9e3e80e88ce5092663902
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-04-04 14:05:17 -0300

    Update aluno.h

--- estrutura I/Estrutura I e Lab I: 2-Unidade/estudando prova-c/tentiva_trabalho-c/aluno.h
+++ estrutura I/Estrutura I e Lab I: 2-Unidade/estudando prova-c/tentiva_trabalho-c/aluno.h
@@ -1,6 +1,7 @@
+
 typedef struct aluno Aluno;
 
-int ler_aluno(Aluno *a);
+void ler_aluno(Aluno *a);
 
 void menu(void);
 
@@ -9,3 +10,7 @@ void imprime_alunos(Aluno* a, int qtd_alunos);
 void criar_alunos(Aluno* a, int qtd_alunos);
 
 int busca_interpolada_mat(Aluno* a,int qtd_alunos, int mat);
+
+int busca_interpolada_nome(Aluno* a,int qtd_alunos, char* nome);
+
+void salva_alunos(Aluno* a,int qtd_alunos);
