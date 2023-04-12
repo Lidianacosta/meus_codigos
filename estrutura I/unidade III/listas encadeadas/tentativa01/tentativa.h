43db99220d257bfba7316a3858a2c42c677c6c2c
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-04-12 13:09:51 -0300

    Update tentativa.h

--- estrutura I/unidade III/listas encadeadas/tentativa01/tentativa.h
+++ estrutura I/unidade III/listas encadeadas/tentativa01/tentativa.h
@@ -1,1 +1,14 @@
+typedef struct lista Lista;
+typedef struct listano ListaNo;
 
+Lista* lst_cria (void);
+
+void lst_insere (Lista* l, int v);
+
+void lst_imprime (Lista* l);
+
+int lst_pertence (Lista* l, int v);
+
+void lst_insere_ordenado (Lista* l, int v);
+
+void lst_retira (Lista* l, int v);
