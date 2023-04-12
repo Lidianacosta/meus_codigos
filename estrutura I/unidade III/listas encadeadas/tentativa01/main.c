a44cecf9bc8fb7a21025a24bce5eeaf3ab1d81b6
Lidiana costa <116831875+Lidianacosta@users.noreply.github.com>
2023-04-12 13:08:23 -0300

    Update main.c

--- estrutura I/unidade III/listas encadeadas/tentativa01/main.c
+++ estrutura I/unidade III/listas encadeadas/tentativa01/main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tentativa01.c"
 
+int main(void){
+
+    Lista* l = lst_cria();
+    lst_insere(l,23);
+    lst_insere(l,45);
+    lst_imprime(l);
+    int verifica = lst_pertence(l,23);
+
+    if (verifica == 1){
+        printf("pertenceeee\n");
+    }else {
+        printf("nao pertence\n");
+    }
+    
+    return 0;
+}
